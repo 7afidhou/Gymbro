f26f0c730374c17d60e1161761bfef3308bcca6f
7afidhou <abdelhafidhhaouari2@gmail.com>
2024-07-10 23:53:06 +0200

    new files

--- src/parts/hero.jsx
+++ src/parts/hero.jsx
@@ -7,7 +7,7 @@ export default function Hero() {
     <div className='picture h-screen w-full bg-no-repeat font-Anton '>
       <div className='h-full w- bg-gray-600/50'>
         <nav className='flex justify-between items-center px-6  md:px-12'>
-          <a className=' text-2xl md:text-4xl p-4 cursor-pointer'>Gymify</a>
+          <a className=' text-2xl md:text-4xl p-4 cursor-pointer'>GymBro</a>
           <div className='flex text-xl md:text-3xl gap-4 md:gap-12 text-white'>
             <Link>getting started</Link>
             <Link>about</Link>
